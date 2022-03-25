2bfa4003f8a2fe216e38ae1ee9903a398e180046
PiscisAustrinu <2862450172@qq.com>
2022-03-25 10:07:07 +0800

    My Study Code

--- SpringBoot/SpringBoot-demo1/src/main/java/com/Ylb/SpringBoot/MainApplication.java
+++ SpringBoot/SpringBoot-demo1/src/main/java/com/Ylb/SpringBoot/MainApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * Author：Yang Lingbo
  * Date:2022/3/24 20:53
- * Description:
+ * Description: @SpringBootApplication 这是一个SpringBoot应用
  */
 @SpringBootApplication
 public class MainApplication {
