1f4adf9900740b17a160be0ae96f7fca566e6e27
PiscisAustrinu <2862450172@qq.com>
2022-03-27 16:12:52 +0800

    使用SpringBoot Initializr创建SpringBoot工程

--- SpringBoot/SpringBoot-demo1/src/main/java/com/Ylb/SpringBoot/controller/HelloController.java
+++ SpringBoot/SpringBoot-demo1/src/main/java/com/Ylb/SpringBoot/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.Ylb.SpringBoot.controller;
 
 import com.Ylb.SpringBoot.bean.Phone;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * Date:2022/3/24 21:01
  * Description:
  */
+@Slf4j
 @RestController
 public class HelloController {
     @Autowired
@@ -21,6 +23,7 @@ public class HelloController {
 
     @RequestMapping("/hello")
     private String handler01(){
+        log.info("请求进来了");
         return "hello,Spring Boot2";
     }
 }
