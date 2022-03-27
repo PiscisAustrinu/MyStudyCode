32e4bb63e0241699f2aa3b198bcea41bc8f157d4
PiscisAustrinu <2862450172@qq.com>
2022-03-27 16:13:46 +0800

    使用SpringBoot Initializr创建SpringBoot工程

---
+++ SpringBoot-Initializr/src/main/java/com/Ylb/boot/SpringBootInitializrApplication.java
@@ -0,0 +1,13 @@
+package com.Ylb.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootInitializrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootInitializrApplication.class, args);
+    }
+
+}
